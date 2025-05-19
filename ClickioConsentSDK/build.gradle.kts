f187cd311b73e12e27f4a0cc1321fb0e88425157
Sergey Shtukin <sergey.shtukin.work@gmail.com>
2025-05-19 19:38:13 +0200

    increase version

--- ClickioConsentSDK/build.gradle.kts
+++ ClickioConsentSDK/build.gradle.kts
@@ -49,7 +49,7 @@ mavenPublishing {
 }
 
 mavenPublishing {
-    coordinates("com.clickio", "clickioconsentsdk", "1.0.0-rc6")
+    coordinates("com.clickio", "clickioconsentsdk", "1.0.0-rc7")
 
     pom {
         name.set("ClickioConsentSDK")
